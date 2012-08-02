805b655a826d4c836d3f159aa17400d22a530bfd
tobyspark <tobyspark@localhost>
2012-08-02 13:28:35 +0000

    MIT License as per MBED guideline

--- spk_tvone.h
+++ spk_tvone.h
@@ -1,7 +1,24 @@
 // *spark audio-visual and v002
 // RS232 Control for TV-One products
 // Good for 1T-C2-750, others will need some extra work
-// Copyright *spark audio-visual / v002 2009-2011
+
+/* Copyright (c) 2009 Toby Harris and Anton Marini, MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #ifndef SPKTVOne_h
 #define SPKTVOne_h
